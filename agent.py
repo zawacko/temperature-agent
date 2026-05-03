c8308f56b9641e45a8eb0cc38a98596e78a4fbf8
zawacko <zach.s.cho@gmail.com>
2026-05-03 16:05:15 -0700

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,1 +1,9 @@
+import requests
 
+url = "https://api.open-meteo.com/v1/forecast?latitude=37.3&longitude=-121.9&current_weather=true"
+
+response = requests.get(url)
+
+data = response.json()
+
+print(data)
